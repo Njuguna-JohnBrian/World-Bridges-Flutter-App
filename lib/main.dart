2dade958b871157624dc3573a6466ef8e7ac50b5
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-06-29 10:33:49 +0300

    create splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/landing_page.dart';
 import 'screens/splash_page_screen.dart';
 
 
--- lib/screens/splash_page_screen.dart
+++ lib/screens/splash_page_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:world_bridges/colors.dart';
+import 'package:world_bridges/screens/landing_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const LandingPage(),
+          ),
+        );
+      },
+    );
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(color: mainYellow),
+        const Align(
+          alignment: Alignment.center,
+          child: Icon(
+            Icons.airplanemode_active,
+            color: Colors.black,
+            size: 90,
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 160,
+            height: 160,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                Colors.black.withOpacity(0.2),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
